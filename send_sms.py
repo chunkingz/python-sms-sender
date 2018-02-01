6103b1eaddaaa274d06bd1f25800b784661513eb
chun_kingz <fortuneking33@yahoo.com>
2018-02-01 15:35:08 +0100

    Add files via upload

---
+++ send_sms.py
@@ -0,0 +1,17 @@
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = ""
+
+# Your Auth Token from twilio.com/console
+auth_token  = ""
+
+client = Client(account_sid, auth_token)
+
+# To and From are phone numbers
+message = client.messages.create(
+    to="", 
+    from_="",
+    body="My name is chunKing")
+
+print(message.sid)
